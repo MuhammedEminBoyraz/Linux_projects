3db8e22287bf9f3ecb662ca7dc14eebbb0cf5cc2
MuhammedEminBoyraz <muhammedeminboyraz10@gmail.com>
2026-03-01 15:28:47 +0300

    SİSTEM İZLEME SCRİPTİ TAMAMLANDI

--- sistem_izleme/sistem_izleme.sh
+++ sistem_izleme/sistem_izleme.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+echo "========== SİSTEM RAPORU =========="
+echo "Tarih: $(date)"
+echo ""
+echo "--- CPU KULLANIMI ---"
+top -bn1 | grep "Cpu(s)"
+echo ""
+echo "--- RAM KULLANIMI ---"
+free -h
+echo ""
+echo "--- DİSK KULLANIMI ---"
+df -h
+echo "==================================="
+
